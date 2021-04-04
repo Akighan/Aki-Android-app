045a0ae57cd728b9784b159124e920fc3a70afb7
Akighan <Akighan@mail.ru>
2021-04-04 20:48:53 +0300

    add settings

--- app/src/main/java/com/github/akighan/aki/SettingsFragment.java
+++ app/src/main/java/com/github/akighan/aki/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.github.akighan.aki;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -15,9 +17,13 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,10 +32,16 @@ import com.github.akighan.aki.server.LaptopServer;
 
 
 public class SettingsFragment extends Fragment {
+    private Switch telegramOnSwitch;
+    private Switch weatherNotification;
+    private Spinner citySpinner;
+    private Button saveButton;
+    private LinearLayout weatherNotificationContainer;
+    public static final String APP_PREFERENCES = "settings";
+    public static final String APP_TELEGRAM_ASSISTANT = "TELEGRAM_ASSISTANT";
+    public static final String APP_WEATHER_NOTIFICATION = "WEATHER_NOTIFICATION";
+    public static final String APP_CITY_FOR_WEATHER = "CITY_FOR_WEATHER";
 
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,25 +51,133 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        telegramOnSwitch = view.findViewById(R.id.telegram_on_button);
+        weatherNotification = view.findViewById(R.id.sw_weather_notification);
+        citySpinner = view.findViewById(R.id.sp_choose_city);
+        weatherNotificationContainer = view.findViewById(R.id.weather_notification_layout);
+        saveButton = view.findViewById(R.id.bt_save_changes);
+
+        loadPreferences();
+
+        onClickListener(saveButton);
+        onCheckedChangeListener(telegramOnSwitch);
+        onCheckedChangeListener(weatherNotification);
+        setOnItemSelectedListener(citySpinner);
+
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Switch telegramOnSwitch = view.findViewById(R.id.telegram_on_button);
-        telegramOnSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    }
+
+    private void onClickListener(Button saveButton) {
+        saveButton.setOnClickListener(view -> {
+            SharedPreferences sharedPreferences = SettingsFragment.this.getActivity()
+                    .getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+            boolean hasVisited = sharedPreferences.getBoolean("hasVisited", false);
+            final String ANDROID_ID = Settings.Secure.getString(getContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+            if (!hasVisited) {
+                sharedPreferences.edit().putBoolean("hasVisited", true).apply();
+                new Thread(() -> savePreferences(ANDROID_ID)).start();
+                Intent browserIntent = new
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/AkighanBot?start=" + ANDROID_ID));
+                startActivity(browserIntent);
+            } else {
+                new Thread(() -> savePreferences(ANDROID_ID)).start();
+
+                NavHostFragment.findNavController(this).navigate(R.id.action_settingsFragment_to_mainFragment);
+            }
+        });
+
+    }
+
+    private void savePreferences(String clientId) {
+        SharedPreferences sharedPreferences = SettingsFragment.this.getActivity()
+                .getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(APP_TELEGRAM_ASSISTANT, telegramOnSwitch.isChecked())
+        .putBoolean(APP_WEATHER_NOTIFICATION, weatherNotification.isChecked())
+        .putInt(APP_CITY_FOR_WEATHER,citySpinner.getSelectedItemPosition()).apply();
+        LaptopServer laptopServer = new LaptopServer();
+
+        try {
+            laptopServer.sendSettings(clientId,sharedPreferences);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setOnItemSelectedListener(Spinner citySpinner) {
+        citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            int num =0; //this need to avoid SharedPreferences load
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    final String ANDROID_ID = Settings.Secure.getString(getContext().getContentResolver(), Settings.Secure.ANDROID_ID);
-                    Intent browserIntent = new
-                            Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/AkighanBot?start="+ANDROID_ID));
-                    startActivity(browserIntent);
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                num++;
+                if (num>1) {
+                    activateSaveButton();
                 }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
     }
+
+
+    private void onCheckedChangeListener(Switch sw) {
+        if (sw.getId() == telegramOnSwitch.getId()) {
+            sw.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                activateSaveButton();
+                if (isChecked) {
+                    weatherNotificationContainer.setVisibility(View.VISIBLE);
+                    if (weatherNotification.isChecked()) {
+                        citySpinner.setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    weatherNotificationContainer.setVisibility(View.INVISIBLE);
+                    citySpinner.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else if (sw.getId() == weatherNotification.getId()) {
+            sw.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                activateSaveButton();
+                if (isChecked) {
+                    citySpinner.setVisibility(View.VISIBLE);
+                } else citySpinner.setVisibility(View.INVISIBLE);
+            });
+        }
+    }
+
+    private void loadPreferences() {
+        boolean isTelegramChecked;
+        boolean isWeatherNotificationChecked = false;
+        int cityChosen;
+        SharedPreferences sharedPreferences = SettingsFragment.this.getActivity()
+                .getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        isTelegramChecked = sharedPreferences.getBoolean(APP_TELEGRAM_ASSISTANT, false);
+        telegramOnSwitch.setChecked(isTelegramChecked);
+        if (isTelegramChecked) {
+            isWeatherNotificationChecked = sharedPreferences.getBoolean(APP_WEATHER_NOTIFICATION, false);
+            weatherNotification.setChecked(isWeatherNotificationChecked);
+            weatherNotificationContainer.setVisibility(View.VISIBLE);
+        }
+        if (isWeatherNotificationChecked) {
+            cityChosen = sharedPreferences.getInt(APP_CITY_FOR_WEATHER, 0);
+            citySpinner.setSelection(cityChosen);
+            citySpinner.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void activateSaveButton() {
+        saveButton.setEnabled(true);
+        saveButton.setClickable(true);
+        saveButton.setVisibility(View.VISIBLE);
+    }
+
 
     @Override
     public void onPrepareOptionsMenu(@NonNull Menu menu) {
--- app/src/main/java/com/github/akighan/aki/server/LaptopServer.java
+++ app/src/main/java/com/github/akighan/aki/server/LaptopServer.java
@@ -1,7 +1,9 @@
 package com.github.akighan.aki.server;
 
+import android.content.SharedPreferences;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.github.akighan.aki.MainActivity;
@@ -14,31 +16,37 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
 
+import static com.github.akighan.aki.SettingsFragment.APP_CITY_FOR_WEATHER;
+import static com.github.akighan.aki.SettingsFragment.APP_TELEGRAM_ASSISTANT;
+import static com.github.akighan.aki.SettingsFragment.APP_WEATHER_NOTIFICATION;
+
 public class LaptopServer {
     private static final String LOG_TAG = "Aki";
     private String mServerName = "192.168.1.44";
     private int mServerPort = 6789;
     private Socket mSocket = null;
     private DBSingleton dbSingleton = DBSingleton.getInstance();
+    public static final String SEND_NOTES_MODIFICATION = "-N";
+    public static final String SEND_SETTINGS_MODIFICATION = "-S";
 
     public LaptopServer() {
     }
 
-    public void openConnection () throws Exception {
+    public void openConnection() throws Exception {
         closeConnection();
         try {
-            mSocket = new Socket(mServerName,mServerPort);
+            mSocket = new Socket(mServerName, mServerPort);
         } catch (IOException e) {
             throw new Exception("Невозможно создать соединение" + e.getMessage());
         }
     }
 
-    public void closeConnection () {
+    public void closeConnection() {
         if (mSocket != null && !mSocket.isClosed()) {
             try {
                 mSocket.close();
             } catch (IOException e) {
-                Log.e (LOG_TAG, "Невозможно закрыть сокет:" + e.getMessage());
+                Log.e(LOG_TAG, "Невозможно закрыть сокет:" + e.getMessage());
             } finally {
                 mSocket = null;
             }
@@ -52,7 +60,7 @@ public class LaptopServer {
         closeConnection();
     }
 
-    public void sendData (String clientId) throws Exception {
+    public void sendData(String clientId) throws Exception {
         openConnection();
         if (mSocket == null || mSocket.isClosed()) {
             throw new Exception("Невозможно отправить данные. Сокет не создан или закрыт");
@@ -61,15 +69,44 @@ public class LaptopServer {
         try {
             List<Note> sendList = NotesReceiver.getInstance().getNotes();
             StringBuilder packedCollection = new StringBuilder();
+
             packedCollection.append(clientId).append('\n');
-            for (int i = 0; i<sendList.size();i++) {
+            packedCollection.append(SEND_NOTES_MODIFICATION).append('\n');
+
+            for (int i = 0; i < sendList.size(); i++) {
                 packedCollection.append(sendList.get(i).getNote()).append('\n');
             }
+
             mSocket.getOutputStream().write(packedCollection.toString().getBytes());
             mSocket.getOutputStream().flush();
-        } catch (IOException e){
+        } catch (IOException e) {
             throw new Exception("Невозможно отправить данные" + e.getMessage());
         }
+    }
 
+    public void sendSettings (String clientId, SharedPreferences sharedPreferences) throws Exception {
+        openConnection();
+        if (mSocket == null || mSocket.isClosed()) {
+            throw new Exception("Невозможно отправить данные. Сокет не создан или закрыт");
+        }
+
+        try {
+            StringBuilder packedSettings = new StringBuilder();
+
+            boolean isTelegramChecked = sharedPreferences.getBoolean(APP_TELEGRAM_ASSISTANT, false);
+            boolean isWeatherNotificationChecked = sharedPreferences.getBoolean(APP_WEATHER_NOTIFICATION, false);
+            int cityChosen = sharedPreferences.getInt(APP_CITY_FOR_WEATHER, 0);
+
+            packedSettings.append(clientId).append('\n');
+            packedSettings.append(SEND_SETTINGS_MODIFICATION).append('\n');
+            packedSettings.append(isTelegramChecked).append('\n');
+            packedSettings.append(isWeatherNotificationChecked).append('\n');
+            packedSettings.append(cityChosen).append('\n');
+
+            mSocket.getOutputStream().write(packedSettings.toString().getBytes());
+            mSocket.getOutputStream().flush();
+        } catch (IOException e) {
+            throw new Exception("Невозможно отправить данные" + e.getMessage());
+        }
     }
 }
